fed12aa64031dcc4289528e6255ad08c10aedddd
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-01-29 18:23:27 +0700

    update

--- 2.yolo8_OBB.py
+++ 2.yolo8_OBB.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8n.pt')  # load an official model
+model = YOLO('yolov8n-obb.pt')  # load an official model
 
 # Predict with the model
-results = model('bus.jpg', save=True, project='81. yolov8 project', name='OBB')
+results = model('plane.jpg', save=True, project='81. yolov8 project', name='OBB')
