8d7d00e8a4c2c546f92531d00754a88717ac289d
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-02-18 07:27:06 +0700

    update

--- 2.yolo8_OBB.py
+++ 2.yolo8_OBB.py
@@ -5,3 +5,5 @@ model = YOLO('yolov8n-obb.pt')  # load an official model
 
 # Predict with the model
 results = model('plane.jpg', save=True, project='81. yolov8 project', name='OBB')
+for result in results:
+    print(result.obb.xyxyxyxy)
