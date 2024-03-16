5e87a010cc33b0980cd43ccc28bdc1752079a1e1
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-03-16 19:37:10 +0700

    update

--- 3.OBB_label_tool.py
+++ 3.OBB_label_tool.py
@@ -1,21 +1,46 @@
-import cv2
-import numpy as np
+import sys
+import math
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel
 
-# Đọc bức ảnh
-image_path = 'plane.jpg'
-image = cv2.imread(image_path)
 
-# Vẽ hình chữ nhật (ví dụ: góc trái trên (100, 100), góc phải dưới (200, 200))
-cv2.rectangle(image, (100, 100), (200, 200), (255, 0, 0), 2)  # Màu xanh lá cây, độ dày viền 2
+class MeinWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setGeometry(200, 50, 300, 300)
+        self.pen1 = QtGui.QPen(QtGui.QColor(0, 0, 0))
+        self.pen2 = QtGui.QPen(QtGui.QColor(255, 0, 0))
+        self.pen3 = QtGui.QPen(QtGui.QColor(0, 255, 0))
+        self.pen4 = QtGui.QPen(QtGui.QColor(0, 0, 255))
+        self.brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
 
-# Xoay hình chữ nhật (ví dụ: xoay 45 độ)
-center = (150, 150)  # Tọa độ tâm của hình chữ nhật
-angle = 45  # Góc xoay
-scale = 1  # Tỉ lệ không thay đổi
-rotate_matrix = cv2.getRotationMatrix2D(center=center, angle=angle, scale=scale)
-rotated_rect = cv2.warpAffine(image, rotate_matrix, (image.shape[1], image.shape[0]))
+        self.pens = (self.pen1, self.pen2, self.pen3, self.pen4)
+        self.rw = 100
+        self.rh = 50
 
-# Hiển thị ảnh
-cv2.imshow('Rotated Rectangle', rotated_rect)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        self.label = QLabel(self)
+        self.label.setGeometry(200, 50, 300, 300)
+        pixmap = QPixmap("plane.jpg")  # Đường dẫn đến tệp hình ảnh
+        self.label.setPixmap(pixmap)
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.translate(QtCore.QPointF(self.rw, self.rh))  # move rotation center to an arbitrary point of widget
+        angle = 10
+        for i in range(0, len(self.pens)):
+            dy = self.rh - self.rh * math.cos(math.radians(angle))  # vertical offset of bottom left corner
+            dx = self.rh * math.sin(math.radians(angle))  # horizontal offset of bottom left corner
+            p = self.pens[i]
+            p.setWidth(3)
+            painter.setPen(p)
+            painter.drawRect(0, 0, self.rw, self.rh)
+            painter.translate(QtCore.QPointF(dx, dy))  # move the wanted rotation center to old position
+            painter.rotate(angle)
+            angle += 20
+
+
+app = QtWidgets.QApplication(sys.argv)
+widget = MeinWidget()
+widget.show()
+sys.exit(app.exec_())
