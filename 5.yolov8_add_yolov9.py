7242ee5a40998481194c33b83d0776f163162e67
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-06-23 08:15:39 +0700

    update

--- 5.yolov8_add_yolov9.py
+++ 5.yolov8_add_yolov9.py
@@ -3,23 +3,22 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO("yolov10n.pt")
-model = YOLO("yolov9c.pt")
+model = YOLO("yolov9t.pt")
 # model = YOLO("yolov9c-seg.pt")
 
 # Use the model
 results = model("video1.mp4", stream=True)  # predict on an image
 for result in results:
+    # original image
+    orig_img = result.orig_img
+    # after predict image
+    predict_img = result.plot()
     if result:
-        # original image
-        orig_img = result.orig_img
-        # after predict image
-        predict_img = result.plot()
-
         boxes = result.boxes.numpy()  # Boxes object for bbox outputs
         for box in boxes:  # there could be more than one detection
             print("class", box.cls)
             print("conf", box.conf)
             print("xyxy", box.xyxy)
 
-        cv2.imshow("predict img", predict_img)
-        cv2.waitKey(1)
+    cv2.imshow("predict img", predict_img)
+    cv2.waitKey(1)
