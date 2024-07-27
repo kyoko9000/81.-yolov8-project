497d8ce53404adc543b25fb29f6d822094ebbf30
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-07-28 06:48:01 +0700

    update

---
+++ 10.fix_location.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="coco8.yaml", epochs=1)
+
+# from ultralytics import settings
+
+# Update a setting
+# settings.update({"datasets_dir": "D:\\2.Python_projects\\81.-yolov8-project\\datasets"})
+
+# # Reset settings to default values
+# settings.reset()
+
+# View all settings
+# print(settings)
