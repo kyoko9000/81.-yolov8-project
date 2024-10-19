7f661265e2185bad5cdbee2713eda30e2b28abae
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-10-20 05:39:10 +0700

    update

--- 7.yolov8_counting_v1.py
+++ 7.yolov8_counting_v1.py
@@ -118,7 +118,7 @@ class live_stream(QThread):
         self.region_points = [(300, 400), (1280, 404), (1280, 360), (300, 360)]
 
         # Init Object Counter
-        counter = ObjectCounter(show=True,
+        counter = ObjectCounter(show=False,
                                 region=self.region_points,
                                 model=model)
 
@@ -127,14 +127,14 @@ class live_stream(QThread):
             if not success:
                 print("Video frame is empty or video processing has been successfully completed.")
                 break
-            im0 = counter.count(im0)
+            im1 = counter.count(im0)
 
             # draw rectangle
             color = (255, 0, 0)
             thickness = 2
             start_point = self.region_points[3]
             end_point = self.region_points[1]
-            image = cv2.rectangle(im0, start_point, end_point, color, thickness)
+            image = cv2.rectangle(im1, start_point, end_point, color, thickness)
 
             self.signal.emit(image)
             if self.stop:
@@ -162,34 +162,3 @@ if __name__ == "__main__":
     main_win = MainWindow()
     main_win.show()
     sys.exit(app.exec())
-
-# from ultralytics import YOLO
-# from ultralytics.solutions import object_counter
-# import cv2
-#
-# model = YOLO("yolov8n.pt")
-# cap = cv2.VideoCapture("video1.mp4")
-# assert cap.isOpened(), "Error reading video file"
-# # w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
-#
-# # Define region points
-# region_points = [(300, 400), (1280, 404), (1280, 360), (300, 360)]
-#
-# # Init Object Counter
-# counter = object_counter.ObjectCounter()
-# counter.set_args(view_img=True,
-#                  reg_pts=region_points,
-#                  classes_names=model.names,
-#                  draw_tracks=True,
-#                  line_thickness=2)
-#
-# while cap.isOpened():
-#     success, im0 = cap.read()
-#     if not success:
-#         print("Video frame is empty or video processing has been successfully completed.")
-#         break
-#     tracks = model.track(im0, persist=True, show=False, conf=0.5)
-#     im0 = counter.start_counting(im0, tracks)
-#
-# cap.release()
-# cv2.destroyAllWindows()
